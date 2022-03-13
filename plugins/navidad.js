40a5b8c61920f9cf8785694d050429ccacfe2ec2
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 22:28:40 -0600

    The Zen-O-Bot

--- plugins/navidad.js
+++ plugins/navidad.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Navidad 🧑‍🎄*", '©The Shadow Borkers - Bot', '🎄 SIGUIENTE 🎄', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Navidad 🧑‍🎄*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '🎄 SIGUIENTE 🎄', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(navidad)$/i
 module.exports = handler

ceb8a7832e37f50088e7bb46e1ac634d284a44fb
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 23:11:35 -0600

    The Zen-O-Bot

--- plugins/strash.js
+++ plugins/strash.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text }) => {
   let img = await q.download()
   let url = await uploadImage(img)
   let wanted = global.API('dzx', '/api/canvas/trash', { url }) //`https://api.dhamzxploit.my.id/api/canvas/trash?url=${url}`
-  let stiker = await sticker(null, wanted, 'trash', '©The Shadow Brokers - Bot')
+  let stiker = await sticker(null, wanted, 'trash', '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })

a96424bf781f715f74b67d42c0078af8eaa5c9ce
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 20:54:00 -0600

    The Zen-O-Bot

--- plugins/futbol.js
+++ plugins/futbol.js
@@ -5,6 +5,6 @@ let json = res.data
 let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Fútbol 🥅*", '©The Shadow Borkers - Bot', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Fútbol 🥅*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(futbol|fútbol)$/i
 module.exports = handler

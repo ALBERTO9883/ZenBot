28b7f882ed22168722821ff79709b12c9de4c50a
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 22:58:18 -0600

    The Zen-O-Bot

--- plugins/video.js
+++ plugins/video.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
-await conn.sendButtonVid(m.chat, pickRandom(asupan), '🥵🔥', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🥵🔥', '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^video$/i
 module.exports = handler

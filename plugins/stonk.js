96965f30c21591845d2b12b947d7dbc7a4d8df09
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 23:12:23 -0600

    The Zen-O-Bot

--- plugins/stonk.js
+++ plugins/stonk.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text }) => {
     let stonk = global.API('https://hardianto.xyz', '/api/stonk', {
     image: await conn.getProfilePicture(m.sender).catch(_ => ''),
     apikey: 'hardianto'})
-    let stiker = await sticker(null, stonk, 'stonks', '©The Shadow Brokers - Bot')
+    let stiker = await sticker(null, stonk, 'stonks', '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })

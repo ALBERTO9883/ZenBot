2ce6263a741cd1df0e14a38de21b039d9df21d2c
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 22:57:04 -0600

    The Zen-O-Bot

--- plugins/random_cat.js
+++ plugins/random_cat.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn, text }) => {
      let res = await fetch('https://cataas.com/cat')
      let img = await res.buffer()
      let caption = `
-©The Shadow Brokers - Bot
+*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*
 `.trim()
     conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
    } catch (e) {

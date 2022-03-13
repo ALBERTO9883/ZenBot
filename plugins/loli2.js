4fb601842c473ee6d863f3f4ff73b8fa0487e18b
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 22:15:09 -0600

    The Zen-O-Bot

--- plugins/loli2.js
+++ plugins/loli2.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 	let url = anime[Math.floor(Math.random() * anime.length)]
 	await conn.sendMessage(m.chat, {
 		contentText: '*TE VA A CAER LA POLICÍA 🚔️*',
-		footerText: '©The Shadow Brokers - Bot',
+		footerText: '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*',
 		buttons: [
 			{ buttonId: '.loli2', buttonText: { displayText: 'SIGUIENTE' }, type: 1 }
 		],

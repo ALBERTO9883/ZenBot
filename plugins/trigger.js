dc6e5dee64c639f1d46b7e707b8c12614d05b0a0
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 23:00:52 -0600

    The Zen-O-Bot

--- plugins/trigger.js
+++ plugins/trigger.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let wanted = global.API('https://some-random-api.ml', '/canvas/triggered', {
 avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),})
-let stiker = await sticker(null, wanted, 'Trigger', '©The Shadow Brokers - Bot')
+let stiker = await sticker(null, wanted, 'Trigger', '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*')
 conn.sendMessage(m.chat, stiker, MessageType.sticker, {
 quoted: m})}
 handler.command = /^(trigger)$/i

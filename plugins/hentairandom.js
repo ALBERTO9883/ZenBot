8aaf546688013aca8c5570f139e206d612c28a98
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 20:59:16 -0600

    The Zen-O-Bot

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai By ShadowBot*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hentai By ZenBot*', '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 

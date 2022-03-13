06f964e58766f292fafd1b9b69ea313e2c26648b
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-12 22:05:28 -0600

    The Zen-O-Bot

--- plugins/yaoi1.js
+++ plugins/yaoi1.js
@@ -2,6 +2,6 @@ let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix, command }) => {
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yaoi?&apikey=ConfuMods`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
 handler.command = /^(yaoi)$/i
 module.exports = handler

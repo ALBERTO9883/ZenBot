e2feb0abcfc10a9792c2d6517546b2a3aca7ebff
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-03-23 15:01:23 -0600

    The Zen-O-Bot

--- plugins/no etiquetar.js
+++ plugins/no etiquetar.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@5219996125657|@17722386341/i
+handler.customPrefix = /@50499698072|@50246028932/i
 handler.command = new RegExp
 
 module.exports = handler

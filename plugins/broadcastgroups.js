e76ee993aa999300694f1f764230655c2857cf62
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-06 15:53:50 -0600

    The Zen-O-Bot

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,20 +1,23 @@
-let handler  = async (m, { conn, text }) => {
-  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message).map(v => v.jid)
-  let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
-  for (let id of groups) conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? {
-    contextInfo: m.msg.contextInfo
-  } : {})
-  conn.reply(m.chat, `_Envíando el mensaje de difusión a ${groups.length} grupos_`, m)
-}
-handler.help = ['broadcastgroup','bcgc'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^(broadcast|bc)(group|grup|gc)$/i
-handler.rowner = true
+let fs = require ('fs')
 
-handler.fail = null
-
-module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
+let handler  = async (m, { conn, text, participants }) => {
+	  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message).map(v => v.jid)
+	  let faketumb = fs.readFileSync('./storage/image/menu2.jpg')
+	  let fakereply = { key : { participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2022, status: 1, surface : 1, message: '↷✦╎Anuncio a Grupos╎💌˖ ⸙', orderTitle: "↷✦╎Anuncio a Grupos╎💌˖ ⸙", thumbnail: faketumb, sellerJid: '0@s.whatsapp.net' } } }
+	  //let content = (/bcgc|broadcastgroup|bcgrup|bcgrup|broadcastgc/i.test(text) ? text : text + '\n' + readMore + '「 ' + conn.getName(conn.user.jid) + ' Mensaje de difusion en grupos 」')
+	  for (let id of groups) await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, m.quoted.id), false, { quoted: fakereply, contextInfo: { mentionedJid: (await conn.groupMetadata(`${id}`)).participants.map(v => v.jid) }})
+	  //conn.sendMessage(id, content, m.mtype, m.msg.contextInfo ? { contextInfo: m.msg.contextInfo } : {})
+	  conn.reply(m.chat, `El mensaje se envío a *${groups.length} grupos*!`, m)
+	}
+	
+	handler.help = ['bcgc']
+	handler.tags = ['owner']
+	handler.command = /^(broadcastgroup|bcgc)$/i
+	handler.rowner = true
+	
+	handler.fail = null
+	
+	module.exports = handler
+	
+	const more = String.fromCharCode(8206)
+	const readMore = more.repeat(4001)

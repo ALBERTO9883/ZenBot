68f40a7c88606b0a322c8f3a4bb31540301b1c13
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-14 09:28:43 -0600

    The Zen-O-Bot

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `tag orangnya!`
+    if (!who) throw `*[ ⚠️ ] Etiquete a una persona!*`
     if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
     global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} sekarang premium!`, m, {
+    conn.reply(m.chat, `@${who.split`@`[0]} Ahora eres premium!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }

291251a65a86d20448f0a94f981187580e3bbb45
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-17 16:09:58 -0600

    The Zen-O-Bot

--- plugins/gay.js
+++ plugins/gay.js
@@ -5,6 +5,8 @@ _*${text}* *es 🏳️‍🌈* *${Math.floor(Math.random() * 200)}%* *${command.
 `.trim(), m, m.mentionedJid ? {
 contextInfo: {
 mentionedJid: m.mentionedJid}} : {})}
+handler.help = ['gay *@user*']
+handler.tags = ['game']
 handler.command = /^(gay|lesbi)/i
 handler.fail = null
 module.exports = handler

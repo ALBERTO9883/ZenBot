c456ffed5311f2541b3d131d5965d59bbe540cae
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-18 11:24:25 -0600

    Add files via upload

--- plugins/play1.js
+++ plugins/play1.js
@@ -1,35 +1,44 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text }) => {
-if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Juan Solo - Querido Corazón*'
-let results = await yts(text)
-let vid = results.all.find(video => video.seconds < 3600)
-if (!vid) throw '*Video/Audio No encontrado* '
-let isVideo = /2$/.test(command)
-let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
-conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*🍁Título: ${title}*
-⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
-*📂Tamaño del archivo: ${filesizeF}*
-⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯
-*🪴 _AGUARDE UN MOMENTO_🪴*
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+  if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Juan Solo - Querido Corazón*'
+  let results = await yts(text)
+   let fs = require('fs')
+ let y = fs.readFileSync('./Menu2.jpg')
+ 
+ 
+  conn.sendMessage(m.chat, `*Aguarde un momento` , 'conversation', {quoted: m, thumbnail: global.thumb, contextInfo:{externalAdReply: {title: 'Simple WhatsAppp Bot', body: `© ${conn.user.name}`, sourceUrl: 'enviando...', thumbnail: y}}})
+  let vid = results.all.find(video => video.seconds < 3600)
+  if (!vid) throw '*Video/Audio No encontrado* '
+  let isVideo = /2$/.test(command)
+  let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
+  //let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesizesLimit
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*⏯ ️Reproductor By Zen-Bot ⏯️*
+
+*${title}*
+*⇄ㅤ     ◁   ㅤ  ❚❚ㅤ     ▷ㅤ     ↻*
+
+*📂Tamaño del archivo:* ${filesizeF}
+*👉🏻Aguarde un momento en lo que envío su audio/video*
 `.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-*🍁Título:* *${title}*
-*📂Tamaño del archivo:* *${filesizeF}*
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-`.trim(), m, false, _thumb || {})}
-handler.help = ['play *música*' , 'play2 *música*']
+  let _thumb = {}
+  try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+  catch (e) { }
+  conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+*🔥Título:* ${title}
+*📂Tamaño del archivo:* ${filesizeF}
+`.trim(), m, false, _thumb || {})
+}
+handler.help = ['play', 'play2'].map(v => v + ' <canción >')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 handler.group = false
+
+handler.exp = 0
 handler.registrar = false
 handler.limit = false
+
 module.exports = handler

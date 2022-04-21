930a9e85c0df3716ef28a5a94580af4f426bed74
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 01:39:03 -0600

    The Zen-O-Bot

--- plugins/cristiano ronaldo.js
+++ plugins/cristiano ronaldo.js
@@ -6,5 +6,7 @@ let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, url, "*Siiiuuuuuu*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['CristianoRonaldo']
+handler.tags = ['images']
 handler.command = /^(cristianoronaldo)$/i
 module.exports = handler

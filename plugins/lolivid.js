b0edbca2aacecc5b6e6a68d6762d14c82f7eb023
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 01:18:32 -0600

    The Zen-O-Bot

--- plugins/lolivid.js
+++ plugins/lolivid.js
@@ -5,5 +5,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonVid(m.chat, `https://recoders-area.caliph.repl.co/api/lolivid`, "*Loli 😎*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '😵‍💫 SIGUIENTE 😵‍💫', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['lolivid']
+handler.tags = ['images']
 handler.command = /^(lolivid|lolivideo)$/i
 module.exports = handler

54ad9ee6a4d30b1112a4f1e1517caea2091f7ab6
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 00:43:45 -0600

    The Zen-O-Bot

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -3,8 +3,6 @@ let handler = async(m, { conn }) => {
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/pussy')
            conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m)
   }
-handler.help = ['pussy']
-handler.tags = ['images']
 handler.command = /^(pussy)$/i
 handler.owner = false
 handler.mods = false

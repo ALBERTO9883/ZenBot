038595de253ad921f1d1c632756d26aa9aed488e
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 01:13:43 -0600

    The Zen-O-Bot

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -6,5 +6,7 @@ let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, ShadowBot, "*Random img 👽*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['imagenrandom']
+handler.tags = ['images']
 handler.command = /^(imagenrandom)$/i
 module.exports = handler

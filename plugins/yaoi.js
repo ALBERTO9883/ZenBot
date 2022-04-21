d9e2831ae1e29aff0295492ecf27d4a643651e08
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 00:48:37 -0600

    The Zen-O-Bot

--- plugins/yaoi.js
+++ plugins/yaoi.js
@@ -3,8 +3,6 @@ const axios = require('axios')
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/yaoigif')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
-handler.help = ['yaoigif']
-handler.tags = ['images']
 handler.command = /^(yaoigif)$/i
 handler.owner = false
 handler.mods = false

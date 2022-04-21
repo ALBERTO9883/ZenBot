dbf4d53b82d46bc89b7c18c162df531eed7737a6
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 01:07:41 -0600

    The Zen-O-Bot

--- plugins/meme3.js
+++ plugins/meme3.js
@@ -6,5 +6,7 @@ let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, ShadowBot, "*😂😂😂😂😂*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['meme3']
+handler.tags = ['images']
 handler.command = /^(meme3)$/i
 module.exports = handler

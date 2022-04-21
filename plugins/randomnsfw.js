cfd2ddb13cfaa3ca4c167fcc8c8dc982923662fb
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 00:45:00 -0600

    The Zen-O-Bot

--- plugins/randomnsfw.js
+++ plugins/randomnsfw.js
@@ -3,8 +3,6 @@ const axios = require('axios')
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw')
             conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
   }
-handler.help = ['randomnsfw']
-handler.tags = ['images']
 handler.command = /^(randomnsfw)$/i
 handler.owner = false
 handler.mods = false

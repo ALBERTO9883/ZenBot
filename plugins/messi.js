45f71a21d9c325ed930b02c61f6615f78b77eff7
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 01:02:53 -0600

    The Zen-O-Bot

--- plugins/messi.js
+++ plugins/messi.js
@@ -6,5 +6,7 @@ let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, url, "*Messi*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['messi']
+handler.tags = ['images']
 handler.command = /^(messi)$/i
 module.exports = handler

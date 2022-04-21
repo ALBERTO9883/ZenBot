f0cf52207e746079ab4e3ddfd1178f67b09f15e2
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-21 01:21:20 -0600

    The Zen-O-Bot

--- plugins/kpop itzy.js
+++ plugins/kpop itzy.js
@@ -6,5 +6,7 @@ let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, ShadowBot, "*✨Itzy✨*", '*©Cᴏᴍᴘᴀɴʏ Zᴇɴ-O-Bᴏᴛ*', 'SIGUIENTE', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.help = ['kpopitzy']
+handler.tags = ['images']
 handler.command = /^(kpopitzy|itzy)$/i
 module.exports = handler

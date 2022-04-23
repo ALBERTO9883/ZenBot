49cc11bf87fff4def1c3dd29e2ff22c4a8ec6268
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-04-23 09:34:44 -0600

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -8,10 +8,10 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
 
   if (chat.antiLink2 && isGroupLink) {
     await m.reply(`*「 ANTI LINKS 」*\n*Hasta la vista baby👋, ${await this.getName(m.sender)} rompiste las reglas serás exterminado....!!*`)
-    await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
-    await m.reply(`*3!!*`)
-    await m.reply(`*2!!*`)
-    await m.reply(`*1!!*`)
+    //await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
+    //await m.reply(`*3!!*`)
+    //await m.reply(`*2!!*`)
+    //await m.reply(`*1!!*`)
     if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
     if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
